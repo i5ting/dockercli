10e874d6ceae1493235bbf7c14a0e37051b1f92b
狼叔 <langshu.ssl@alibaba-inc.com>
2018-08-02 13:54:24 +0800

    lazycommmit, please ignore

--- remove.sh
+++ remove.sh
@@ -8,9 +8,6 @@ echo $version
 
 fullname=$name":"$version
 
-docker ps -a |grep $fullname | awk '{print $1}' | xargs docker stop
 docker ps -a |grep $fullname | awk '{print $1}' | xargs docker rm
 
-docker build . -t $fullname
-
-docker run -p 8888:3000 --name $name $fullname
+echo "end"
